e1a48f24acbba51bcf1aad7f5a8596b95a89cf42
huangbule <3314308761@qq.com>
2018-06-06 20:52:10 +0800

    init

--- deploy.php
+++ deploy.php
@@ -14,7 +14,7 @@ set('git_tty', true);
 
 // Shared files/dirs between deploys 
 set('shared_files', []);
-set('shared_dirs', []);
+set('shared_dirs', ['./public']);
 
 // Writable dirs by web server 
 set('writable_dirs', []);
@@ -25,7 +25,10 @@ set('writable_dirs', []);
 host('47.97.197.100')
     ->user('root')
     ->set('deploy_path', '/home/test');
-    
+
+
+task('deploy:done', 'composer update');
+after('success', 'deploy:done');
 
 // Tasks
 
